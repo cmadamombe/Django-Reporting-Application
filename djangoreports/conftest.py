7b6999b4ebca327235abbc22a3fda8a29aa4fc08
Chucknorris Madamombe <chuckygari@gmail.com>
2022-05-24 23:26:09 +0200

    first commit

---
+++ djangoreports/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djangoreports.users.models import User
+from djangoreports.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
